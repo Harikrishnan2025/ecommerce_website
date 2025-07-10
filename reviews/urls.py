a73da7af2a07ef5b4adce0cfeaa95bb987a3f36e
Harikrishnan G <haripty1999@gmail.com>
2025-07-10 12:59:25 +0530

    Delete reviews directory

--- reviews/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import CreateReviewView, ProductReviewListView, DeleteReviewView
-
-urlpatterns = [
-    path('add/<int:product_id>/', CreateReviewView.as_view(), name='add-review'),
-    path('product/<int:product_id>/', ProductReviewListView.as_view(), name='product-reviews'),
-    path('delete/<int:product_id>/', DeleteReviewView.as_view(), name='delete-review'),
-]
