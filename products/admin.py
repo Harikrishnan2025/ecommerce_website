fcdec058b6590114d5e80c2786242cc57917604d
Harikrishnan G <haripty1999@gmail.com>
2025-07-10 12:59:10 +0530

    Delete products directory

--- products/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Product,ProductImage,Category
-
-admin.site.register(Product)
-admin.site.register(ProductImage)
-admin.site.register(Category)
-
-# Register your models here.
