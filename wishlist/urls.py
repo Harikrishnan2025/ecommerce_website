2ff2fdbd746f7d9f39f8ba040b2b19114951a023
Harikrishnan G <haripty1999@gmail.com>
2025-07-10 12:59:35 +0530

    Delete wishlist directory

--- wishlist/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import AddToWishlistView,WishListView,RemoveFromWishlistView
-urlpatterns = [
-    path('list/', WishListView.as_view(), name='wishlist'),
-    path('add/<int:product_id>/', AddToWishlistView.as_view(), name='addwishlist'),
-    path('remove/<int:product_id>/', RemoveFromWishlistView.as_view(), name='remove-from-wishlist'),
-
-]
-
